fbf044d02b89df3abf790de3f549952edc4c8335
nuri1977 <nurilacka@gmail.com>
2020-02-27 00:23:30 +0100

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My blog post #{blog}",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was popularised 
+          in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+          and more recently with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum."
+  )
+end
+
+5.times do |skills|
+  Skill.create!(
+    title: "Rails #{skills}",
+    percent_utilized: 15
+  )
+end
+
+9.times do |p_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{p_item}",
+    subtitle: "My greta service",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    main_image: "http://placehold.jp/600x400.png" ,
+    thumb_image: "http://placehold.jp/350x200.png"
+  )
+end
+
+puts "10 blogs created"
+puts "5 skills created"
+puts "9 portfolio items created"
