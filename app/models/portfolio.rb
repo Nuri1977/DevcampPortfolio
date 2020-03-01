147c2445fcb6a8c7ae9464537fe2f54eda22f493
nuri1977 <nurilacka@gmail.com>
2020-03-01 16:49:23 +0100

    Implemented validations, new model Topic and realatioships with blogs

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,10 @@
 class Portfolio < ApplicationRecord
+	validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+
+	after_initialize :set_defaults
+
+	def set_defaults
+    self.main_image ||= "http://placehold.jp/600x400.png"
+    self.thumb_image ||= "http://placehold.jp/350x200.png" 
+	end
 end
---
+++ app/models/topic.rb
@@ -0,0 +1,4 @@
+class Topic < ApplicationRecord
+  validates_presence_of :title
+  has_many :blogs
+end
