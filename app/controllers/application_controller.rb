1c01e12309df5906df0d62d770d610e7b3cd9464
nuri1977 <nurilacka@gmail.com>
2020-03-07 22:52:03 +0100

    Custimized layout file, keywords fo search engine added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,6 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  include DefaultPageContent
 
 end
---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @seo_keywords = "Jordan Hidgens Portfolio"
+  end
+
+end
