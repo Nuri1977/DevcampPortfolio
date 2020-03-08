e8acc84f6eac9f2e073cc0aea0a0a6b1d7b6626d
nuri1977 <nurilacka@gmail.com>
2020-03-08 01:05:39 +0100

    Implemented view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      (link_to "Logout", destroy_user_session_path, method: :delete) +
+      "<br>".html_safe +
+      (link_to "Edit Account",  edit_user_registration_path)
+    else
+      (link_to "Login", new_user_session_path) +
+      "<br>".html_safe +
+      (link_to "Register", new_user_registration_path)
+    end
+  end
 end
