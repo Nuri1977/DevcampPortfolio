1bf163745ea514732f7dd3c0b041e32679a64c36
nuri1977 <nurilacka@gmail.com>
2020-03-08 23:39:01 +0100

    Updated master layout and navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a?(User)
-      (link_to "Logout", destroy_user_session_path, method: :delete) +
-      "<br>".html_safe +
-      (link_to "Edit Account",  edit_user_registration_path)
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: style) +
+      "  ".html_safe +
+      (link_to "Your account",  edit_user_registration_path, class: style)
     else
-      (link_to "Login", new_user_session_path) +
-      "<br>".html_safe +
-      (link_to "Register", new_user_registration_path)
+      (link_to "Login", new_user_session_path, class: style) +
+      " ".html_safe +
+      (link_to "Register", new_user_registration_path, class: style)
     end
   end
 
