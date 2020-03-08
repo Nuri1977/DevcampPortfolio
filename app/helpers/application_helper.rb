b7d8f5330107434b43630bb4b34c919e8a7b3f09
nuri1977 <nurilacka@gmail.com>
2020-03-08 01:27:20 +0100

    Implemented content helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
       (link_to "Register", new_user_registration_path)
     end
   end
+
+  def source_helper
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+
 end
