ccec2c4aac34831726dcd05711bce196574d9f8a
nuri1977 <nurilacka@gmail.com>
2020-03-19 22:54:32 +0100

    Implemented live comments with action cable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,11 @@
 class CommentsController < ApplicationController
-
   def create
     @comment = current_user.comments.build(comment_params)
   end
 
-  private comment_params
+  private
+
+  def comment_params
     params.require(:comment).permit(:content)
   end
-
-  def 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     end
   end
   
+  mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
