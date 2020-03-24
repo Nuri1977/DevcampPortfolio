819cdbf5ca340b64371a5fd74f0106fdd1b45b9e
nuri1977 <nurilacka@gmail.com>
2020-03-24 23:52:34 +0100

    javascript libraries order change, owerwriting errors in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require cable
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
